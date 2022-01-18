c9f7bde10952f55dda7c05644e1fd69b2aadef8a
deept368 <dipt1995@gmail.com>
2022-01-17 18:28:40 -0800

    added logging

--- src/main/java/blockchain/Blockchain.java
+++ src/main/java/blockchain/Blockchain.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Blockchain {
     ArrayList<Block> blocks;
+    private static final char[] hexArray = "0123456789abcdef".toCharArray();
 
     Blockchain(){
         blocks  = new ArrayList<>();
@@ -39,14 +40,29 @@ public class Blockchain {
             }
         }
         return balance;
+
     }
 
-    public void print(){
+    private void print(){
         for(int i=0; i<blocks.size(); i++){
             System.out.println("block " + i);
             System.out.println("sender "+ blocks.get(i).getSender());
             System.out.println("receiver "+ blocks.get(i).getReceiver());
             System.out.println("amount "+ blocks.get(i).getAmount()+"$");
+            System.out.println("Hash "+ bytesToHex(blocks.get(i).getCurrentHash()));
+            System.out.println("********************************************");
+            System.out.println();
+
         }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return String.valueOf(hexChars);
     }
 }
